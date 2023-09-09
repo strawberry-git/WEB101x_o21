8e59be54fd05c622d9cfd3c0c0c5557e5b89992f
strawberry-git <134914379+strawberry-git@users.noreply.github.com>
2023-09-09 22:45:22 +0700

    Add files via upload

---
+++ lab14/script.js
@@ -0,0 +1,9 @@
+let country, continent, population;
+
+country = "VietNam";
+continent = 'Asia';
+population = 99;
+
+ console.log('Country: ' + country );
+ console.log('Continent: ' + continent);
+ console.log('Population: ' + population + ' million.');
